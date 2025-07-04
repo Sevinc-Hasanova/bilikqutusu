f7e521fc923a4c02396a3e18fe02afb8ef773b23
Sevinc-Hasanova <sevinchasanovawork@gmail.com>
2025-07-04 23:05:07 +0400

    Add Home Page design

--- src/Modules/Home/view/HomeScreen.jsx
+++ src/Modules/Home/view/HomeScreen.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import homeImg from '../../../assets/image/homeImg.png'
 
 const HomeScreen = () => {
   return (
-      <div id='home'>
-      <div className="container">
-        HomeScreen
+    <div id='home' >
+      <div className="homeDetails">
+        <p className='homeTitle'>
+        Qarşıdakı uğurun üçün ilk addımı at!
+      </p>
+
+      <p className='homeDescription'>
+Uğurlu bir texniki müsahibə, yalnız bilik deyil — zamanında hazırlıq, praktiki təcrübə və doğru yanaşma tələb edir.
+ </p>
+      <button className='homeStartButton'>
+        Təcrübə imtahanına başla
+      </button>
+
+      <div className="homeImg">
+        <img src={homeImg} alt="home image" />
+      </div>
       </div>
     </div>
   )
